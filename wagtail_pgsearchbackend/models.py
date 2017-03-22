67fbc926c3fdf50ea88a79b9c6215aa51b50124a
Ramon de Jezus <rdejezus@leukeleu.nl>
2017-03-22 13:46:01 +0100

    added backend and models

---
+++ wagtail_pgsearchbackend/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+
+
+class Index(models.Model):
+    # configuration
+
+    content_type = models.ForeignKey(ContentType)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+
+    # title =
+    # content =
+
+    # title_search
+    # content_search
