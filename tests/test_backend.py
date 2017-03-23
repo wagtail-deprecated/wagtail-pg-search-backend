44de43a425b42fd1c1428c4ea9138b2ef357547f
Jaap Roes <jroes@leukeleu.nl>
2017-03-23 11:22:35 +0100

    Expect test_individual_field to fail

--- tests/test_backend.py
+++ tests/test_backend.py
@@ -1,3 +1,5 @@
+import unittest
+
 from django.test import TestCase
 
 from wagtail.wagtailsearch.tests.test_backends import BackendTests
@@ -5,3 +7,15 @@ from wagtail.wagtailsearch.tests.test_backends import BackendTests
 
 class TestPgSearchBackend(BackendTests, TestCase):
     backend_path = 'wagtail_pgsearchbackend.backend'
+
+    def reset_index(self):
+        self.backend.reset_index()
+
+    @unittest.expectedFailure
+    def test_individual_field(self):
+        """
+        Searching in individual fields is not possible using
+        an index model.
+
+        """
+        super(TestPgSearchBackend, self).test_individual_field()
